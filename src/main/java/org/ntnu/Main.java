7f4f01ab830de7bf83aaa6c88735b9551aba3661
Tony Nhat Huy Pham <Tony.n.h.pham@hotmail.com>
2024-03-01 12:43:26 +0100

    Push

--- src/main/java/org/ntnu/Main.java
+++ src/main/java/org/ntnu/Main.java
@@ -1,7 +1,27 @@
 package org.ntnu;
 
+import org.ntnu.booking.MovieTicketClient;
+import org.ntnu.booking.MovieTicketServer;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		MovieTicketServer movieTicketServer = new MovieTicketServer("Dune 2", 10);
+
+
+		//4 new threads
+		Thread t1 = new MovieTicketClient(movieTicketServer, "Tony", 3);
+		Thread t2 = new MovieTicketClient(movieTicketServer, "Jonas", 2);
+		Thread t3 = new MovieTicketClient(movieTicketServer, "Emma", 3);
+		Thread t4 = new MovieTicketClient(movieTicketServer, "Henrik", 4);
+
+
+
+		//Starting 4 threads
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
+
+
 	}
 }
